efec2330adec705d81d294721d95ac09650ac26c
Pascalheid <56368900+Pascalheid@users.noreply.github.com>
2020-06-20 14:35:57 +0200

    Add results for table 3.

--- auxiliary/angrist_table1.py
+++ auxiliary/angrist_table1.py
@@ -1,111 +1,120 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_stata(r"data\cwhsa.dta")
-data["type"] = "TAXAB"
 
-temp_data = pd.read_stata(r"data\cwhsb.dta")
-data = data.append(temp_data)
+def table1():
+    data = pd.read_stata(r"data\cwhsa.dta")
+    data["type"] = "TAXAB"
 
-data = data.loc[(data["year"] > 65) & (data["byr"] >= 50)]
+    temp_data = pd.read_stata(r"data\cwhsb.dta")
+    data = data.append(temp_data)
 
-# create eligibility dummy
-data["eligible"] = 0
-data.loc[
-    ((data["byr"] >= 44) & (data["byr"] <= 50) & (data["interval"] <= 39))
-    | ((data["byr"] == 51) & (data["interval"] <= 25))
-    | (((data["byr"] == 52) | (data["byr"] == 53)) & (data["interval"] <= 19)),
-    "eligible",
-] = 1
+    data = data.loc[(data["year"] > 65) & (data["byr"] >= 50)]
 
-# create ethnicity dummy
-data = pd.concat([data, pd.get_dummies(data["race"], prefix="race")], axis=1)
-data.rename(columns={"race_1": "white", "race_2": "nonwhite"}, inplace=True)
+    # create eligibility dummy
+    data["eligible"] = 0
+    data.loc[
+        ((data["byr"] >= 44) & (data["byr"] <= 50) & (data["interval"] <= 39))
+        | ((data["byr"] == 51) & (data["interval"] <= 25))
+        | (((data["byr"] == 52) | (data["byr"] == 53)) & (data["interval"] <= 19)),
+        "eligible",
+    ] = 1
 
-# mean and weights for nonzeros
-data["earn_nz"] = data["vmn1"] / (1 - data["vfin1"])
-data["wt_nz"] = data["vnu1"] * (1 - data["vfin1"])
+    # create ethnicity dummy
+    data = pd.concat([data, pd.get_dummies(data["race"], prefix="race")], axis=1)
+    data.rename(columns={"race_1": "white", "race_2": "nonwhite"}, inplace=True)
 
-data.loc[data["type"] == "TAXAB", "type"] = "FICA"
-# create variance
-var = data["vsd1"] ** 2
-# variance of nonzero cells
-data["var_nz"] = var * (data["vnu1"] / data["wt_nz"])
+    # mean and weights for nonzeros
+    data["earn_nz"] = data["vmn1"] / (1 - data["vfin1"])
+    data["wt_nz"] = data["vnu1"] * (1 - data["vfin1"])
 
-# adjust index for groupby
-wtmult = pd.DataFrame()
-wtmult["wtmult"] = (
-    1 / data.groupby(["white", "byr", "year", "eligible", "type"])["wt_nz"].sum()
-)
-data = pd.merge(
-    data, wtmult, how="outer", on=["white", "byr", "year", "eligible", "type"]
-)
+    data.loc[data["type"] == "TAXAB", "type"] = "FICA"
+    # create variance
+    var = data["vsd1"] ** 2
+    # variance of nonzero cells
+    data["var_nz"] = var * (data["vnu1"] / data["wt_nz"])
 
-data["var_cm"] = data["wtmult"] * data["var_nz"]
-
-# get groupby weighted means
-data_temp = data.fillna(0)
-
-columns = ["var_cm", "earn_nz"]
-# drop groups where the weight sums to zero
-sum_group_weights = (
-    data_temp.groupby(["white", "byr", "year", "eligible", "type"])["wt_nz"]
-    .sum()
-    .to_frame()
-)
-nonzero_weights_index = sum_group_weights.loc[sum_group_weights["wt_nz"] != 0].index
-data_temp.set_index(["white", "byr", "year", "eligible", "type"], inplace=True)
-data_temp = data_temp.loc[nonzero_weights_index]
-data_temp = data_temp.groupby(["white", "byr", "year", "eligible", "type"]).apply(
-    lambda x: np.average(x[["var_cm", "earn_nz"]], weights=x["wt_nz"], axis=0)
-)
-data_temp = pd.DataFrame(
-    data_temp.to_list(), columns=["var_cm", "earn_nz"], index=data_temp.index
-)
-
-index_eligible = (slice(None), slice(None), slice(None), 1, slice(None))
-index_non_eligible = (slice(None), slice(None), slice(None), 0, slice(None))
-treatment_effect = data_temp.loc[index_eligible, "earn_nz"].reset_index(
-    "eligible", drop=True
-) - data_temp.loc[index_non_eligible, "earn_nz"].reset_index("eligible", drop=True)
-
-standard_errors = (
-    data_temp.loc[index_eligible, "var_cm"].reset_index("eligible", drop=True)
-    + data_temp.loc[index_non_eligible, "var_cm"].reset_index("eligible", drop=True)
-) ** 0.5
-
-table_1 = {}
-for dummy, ethnicity in enumerate(["white", "nonwhite"]):
-    new_dummy = 1 - dummy
-    # extract treatment effect and their standard errors
-    treatment_effect_temp = treatment_effect.loc[new_dummy, :, :, :].reset_index(
-        "white", drop=True
+    # adjust index for groupby
+    wtmult = pd.DataFrame()
+    wtmult["wtmult"] = (
+        1 / data.groupby(["white", "byr", "year", "eligible", "type"])["wt_nz"].sum()
     )
-    standard_errors_temp = standard_errors.loc[new_dummy, :, :, :].reset_index(
-        "white", drop=True
+    data = pd.merge(
+        data, wtmult, how="outer", on=["white", "byr", "year", "eligible", "type"]
     )
 
-    # get dataframes into the right shape for table 1
-    treatment_effect_temp = treatment_effect_temp.unstack(
-        level=["type", "byr"]
-    ).sort_index(level="type", axis=1)
-    treatment_effect_temp["Statistic"] = "Average"
-    treatment_effect_temp.set_index("Statistic", drop=True, append=True, inplace=True)
+    data["var_cm"] = data["wtmult"] * data["var_nz"]
 
-    standard_errors_temp = standard_errors_temp.unstack(
-        level=["type", "byr"]
-    ).sort_index(level="type", axis=1)
-    standard_errors_temp["Statistic"] = "Standard Error"
-    standard_errors_temp.set_index("Statistic", drop=True, append=True, inplace=True)
+    # get groupby weighted means
+    data_temp = data.fillna(0)
 
-    # Create table 1
-    year = np.arange(66, 85)
-    statistic = ["Average", "Standard Error"]
-    index = pd.MultiIndex.from_product([year, statistic], names=["year", "Statistic"])
-    table_1_temp = pd.DataFrame(index=index, columns=treatment_effect_temp.columns)
-    table_1_temp.loc[(slice(None), "Average"), :] = treatment_effect_temp
-    table_1_temp.loc[(slice(None), "Standard Error"), :] = standard_errors_temp
-    table_1_temp = table_1_temp.astype(float).round(2)
-    table_1_temp.fillna("", inplace=True)
+    # drop groups where the weight sums to zero
+    sum_group_weights = (
+        data_temp.groupby(["white", "byr", "year", "eligible", "type"])["wt_nz"]
+        .sum()
+        .to_frame()
+    )
+    nonzero_weights_index = sum_group_weights.loc[sum_group_weights["wt_nz"] != 0].index
+    data_temp.set_index(["white", "byr", "year", "eligible", "type"], inplace=True)
+    data_temp = data_temp.loc[nonzero_weights_index]
+    data_temp = data_temp.groupby(["white", "byr", "year", "eligible", "type"]).apply(
+        lambda x: np.average(x[["var_cm", "earn_nz"]], weights=x["wt_nz"], axis=0)
+    )
+    data_temp = pd.DataFrame(
+        data_temp.to_list(), columns=["var_cm", "earn_nz"], index=data_temp.index
+    )
 
-    table_1[ethnicity] = table_1_temp
+    index_eligible = (slice(None), slice(None), slice(None), 1, slice(None))
+    index_non_eligible = (slice(None), slice(None), slice(None), 0, slice(None))
+    treatment_effect = data_temp.loc[index_eligible, "earn_nz"].reset_index(
+        "eligible", drop=True
+    ) - data_temp.loc[index_non_eligible, "earn_nz"].reset_index("eligible", drop=True)
+
+    standard_errors = (
+        data_temp.loc[index_eligible, "var_cm"].reset_index("eligible", drop=True)
+        + data_temp.loc[index_non_eligible, "var_cm"].reset_index("eligible", drop=True)
+    ) ** 0.5
+
+    table_1 = {}
+    for dummy, ethnicity in enumerate(["white", "nonwhite"]):
+        new_dummy = 1 - dummy
+        # extract treatment effect and their standard errors
+        treatment_effect_temp = treatment_effect.loc[new_dummy, :, :, :].reset_index(
+            "white", drop=True
+        )
+        standard_errors_temp = standard_errors.loc[new_dummy, :, :, :].reset_index(
+            "white", drop=True
+        )
+
+        # get dataframes into the right shape for table 1
+        treatment_effect_temp = treatment_effect_temp.unstack(
+            level=["type", "byr"]
+        ).sort_index(level="type", axis=1)
+        treatment_effect_temp["Statistic"] = "Average"
+        treatment_effect_temp.set_index(
+            "Statistic", drop=True, append=True, inplace=True
+        )
+
+        standard_errors_temp = standard_errors_temp.unstack(
+            level=["type", "byr"]
+        ).sort_index(level="type", axis=1)
+        standard_errors_temp["Statistic"] = "Standard Error"
+        standard_errors_temp.set_index(
+            "Statistic", drop=True, append=True, inplace=True
+        )
+
+        # Create table 1
+        year = np.arange(66, 85)
+        statistic = ["Average", "Standard Error"]
+        index = pd.MultiIndex.from_product(
+            [year, statistic], names=["year", "Statistic"]
+        )
+        table_1_temp = pd.DataFrame(index=index, columns=treatment_effect_temp.columns)
+        table_1_temp.loc[(slice(None), "Average"), :] = treatment_effect_temp
+        table_1_temp.loc[(slice(None), "Standard Error"), :] = standard_errors_temp
+        table_1_temp = table_1_temp.astype(float).round(2)
+        table_1_temp.fillna("", inplace=True)
+
+        table_1[ethnicity] = table_1_temp
+
+    return table_1
---
+++ data/Angrist1990_Table3_mine.do
@@ -0,0 +1,168 @@
+
+cap log close
+log using "DIRECTORY HERE\Angrist1990_Table3.log, replace
+
+*************************************************************************
+* PROGRAM: Angrist1990_Table3.do
+* PROGRAMMER: Simone Schaner (sschaner@mit.edu)
+* PURPOSE: Replicates table 3 from Angrist (1990)
+* DATE CREATED: 7/26/07
+* NOTE: To reproduce table results, uncomment lines 23 and 24. Results for
+*	column 5 vary slightly from published values.
+************************************************************************
+
+clear
+set mem 50m
+set more off
+cd "C:\Users\Pascal\Desktop\Pascal\Uni\Master\4.Semester\microeconometrics-course-project-Pascalheid\data"
+
+
+use cwhsc_new
+
+* UNCOMMENT TO USE UNADJUSTED WEIGHTS
+drop iweight
+ren iweight_old iweight
+
+* MERGE ON CPI INFO TO GO BACK TO REAL VALUES
+	sort year
+	merge year using cpi_angrist1990
+		drop _merge
+
+qui sum cpi if year==78
+	local cpi78=r(mean)
+****************************************************************************************************
+replace cpi=cpi/`cpi78'
+	replace cpi=round(cpi,.001) /*TO MATCH ORIGINAL DEFLATION*/
+g cpi2= cpi^2
+g smplsz= nj-nj0 /*NUMBER NONZEROS IN CELLS*/
+
+keep if year>=81
+*  ONLY KEEP BIRTH COHORTS 1950-1952
+keep if byr>=50 & byr<=52
+
+* BACK OUT VARIANCE FROM IWEIGHTS
+g rlvar= (1/iweight)*smplsz
+g var= rlvar*cpi2
+
+* MAKE REAL EARNINGS
+g nomearn= (earnings*cpi)
+
+* SAME PROCESS AS TABLE 1 TO OBTAIN POINT ESTIMATES OF DIFFERENCES
+
+* DRAFT ELIGIBLE
+g byte eligible=0
+	replace eligible=1 if byr==50 & interval==1 /*CUTOFF=195 -- recoded to 2 intervals here*/
+	replace eligible=1 if byr==51 & interval<=25 /*CUTOFF=125*/
+	replace eligible=1 if (byr==52 | byr==53) & interval<=19  /*CUTOFF=95*/
+
+* RACE
+g byte white= race==1
+
+* GROUPS TO COLLAPSE BY
+egen collby= group(white byr year eligible type)
+
+* SUM WEIGHTS ACROSS GROUP CELLS
+	egen sumwt= sum(smplsz), by(collby)
+
+* WEIGHT FOR VARIANCE
+	g wtmult= 1/(sumwt) /*WOULD USE THIS ONE W/ UNWEIGHTED COLLAPSE SUM STATEMENT -- wt_nz/(sumwt)^2*/
+
+* VARIANCE TO SUM
+	g var_cm= wtmult*var
+
+g numtype= 1 if type=="TAXAB"
+	replace numtype=2 if type=="ADJ"
+	replace numtype=3 if type=="TOTAL"
+
+collapse (mean) var_cm nomearn earnings cpi [w=smplsz], by(white byr year eligible numtype)
+
+egen id= concat(white year numtype byr)
+reshape wide var_cm* nomearn* earnings*, i(id) j(eligible)
+
+drop id
+
+g tag= "f" if numtype==1
+	replace tag= "a" if numtype==2
+	replace tag= "w" if numtype==3
+
+	drop numtype
+egen id= concat(white year byr)
+reshape wide var_cm* nomearn* earnings*, i(id) j(tag) string
+
+
+
+		g cf= nomearn1f-nomearn0f
+		g sef=(var_cm1f+var_cm0f)^.5
+		g cw= nomearn1w-nomearn0w
+		g sew=(var_cm1w+var_cm0w)^.5
+		* one of the solutions
+		g ca= nomearn1a-nomearn0a
+		g sea=(var_cm1a+var_cm0a)^.5
+
+
+	keep year byr white c* se*
+
+expand 2
+	sort byr year
+
+* IMPUTE SIPP PROBABILITIES --- SEE CODE FOR TABLE 2 TO RECREATE NUMBERS
+g sippp= .159 if byr==50
+	replace sippp= .136 if byr==51
+	replace sipp= .105 if byr==52
+
+g sippse= .040 if byr==50
+	replace sippse= .043 if byr==51
+	replace sippse= .050 if byr==52
+* one of the solutions
+g serv_c= ca/(sippp*cpi)
+g serv_se= sea/(sippp*cpi)
+
+	mkmat year c* se* sipp* serv* if white, matrix(whites1)
+	mkmat year c* se* sipp* serv* if white==0, matrix(nonwhites1)
+
+	mat whites=J(rowsof(whites1),7,.)
+	mat nonwhites=J(rowsof(nonwhites1),7,.)
+
+		local top=rowsof(whites1)
+	local top1=`top'-1
+
+	foreach mat in whites nonwhites {
+		mat colnames `mat' = "Cohort" "Year" "FICA" "Adj FICA" ///
+			"W-2"	"pe-pn" "Serv Eff"
+		}
+
+
+foreach mat in whites nonwhites {
+	forval val=1(2)`top1' {
+		mat `mat'[`val',2]=`mat'1[`val',"year"]
+		mat `mat'[`val',3]=`mat'1[`val',"cf"]
+		mat `mat'[`val',4]=`mat'1[`val',"ca"]
+		mat `mat'[`val',5]=`mat'1[`val',"cw"]
+		mat `mat'[`val',7]=`mat'1[`val',"serv_c"]
+		}
+
+	forval val=2(2)`top' {
+		mat `mat'[`val',3]=`mat'1[`val',"sef"]
+		mat `mat'[`val',4]=`mat'1[`val',"sea"]
+		mat `mat'[`val',5]=`mat'1[`val',"sew"]
+		mat `mat'[`val',7]=`mat'1[`val',"serv_se"]
+		}
+
+		mat `mat'[1,1]=1950
+		mat `mat'[9,1]=1951
+		mat `mat'[17,1]=1952
+
+		mat `mat'[1,6]=.159
+		mat `mat'[2,6]=.040
+		mat `mat'[9,6]=.136
+		mat `mat'[10,6]=.043
+		mat `mat'[17,6]=.105
+		mat `mat'[18,6]=.050
+		}
+
+
+
+	mat li whites
+	mat li nonwhites
+
+log close
